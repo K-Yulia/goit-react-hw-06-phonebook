e3cf7558c88b7de7b45387d0f42e57b8d9cd409c
Yulia Kutova <kutova85@gmail.com>
2023-04-12 00:08:15 +0300

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,18 +1,22 @@
-import PropTypes from 'prop-types';
 import { Box, Text, Input } from './Filter.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
 
 export const Filter = ({ value, onChange }) => {
+  // const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const onFilter = e => {
+    dispatch(setFilter(e.target.value));
+  };
   return (
     <div>
       <Box>
         <Text>Find contacts by name</Text>
-        <Input type="text" value={value} onChange={onChange} />
+        <Input type="text" value={filter} onChange={onFilter} />
       </Box>
     </div>
   );
-};
-
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
 };
